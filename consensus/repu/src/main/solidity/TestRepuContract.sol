dccbe59c3ed0d3e4f00f5a0bae729c159e416c4e
Daniel López <danilopez99d@gmail.com>
2023-03-25 10:20:50 +0100

    added proxy address validation when updating contract address

--- consensus/repu/src/main/solidity/TestRepuContract.sol
+++ consensus/repu/src/main/solidity/TestRepuContract.sol
@@ -8,6 +8,7 @@ contract TestRepuContract {
     address[] validators;
     uint256 index;
     address public proxy;
+    uint256 lastBlock;
 
     constructor(address _proxy) {
         validators.push(0x1c21335D5E5D3F675D7eB7e19E943535555Bb291);
@@ -15,6 +16,7 @@ contract TestRepuContract {
         validators.push(0x11F8EBFF1B0fFb4dE7814Cc25430D01149fcDC71);
         index = 0;
         proxy = _proxy;
+        lastBlock = 0;
     }
 
     function nextValidators() public view returns (address[] memory) {
@@ -39,20 +41,36 @@ contract TestRepuContract {
         return validators;
     }
 
+    function getLastBlock() public view returns (uint256){
+        return lastBlock;
+    }
+
     function addValidator(address _addr) public {
         validators.push(_addr);
     }
 
-    function updateValidators() public {
+    function getProxyAddress() public view returns (address, bool) {
+        return (proxy, true);
+    }
+
+    function updateValidators(uint256 _lastBlock) public {
         index++;
+        lastBlock = _lastBlock;
     }
 
     //function deleteValidator(address _addr) public {
     //    validator = _addr;
     //}
 
-    function updateContractAddress(address _new) external{
-        (bool success,bytes memory data) = proxy.call(abi.encodeWithSignature('setConsensusAddress(address)', _new));
+    modifier hasCorrectProxyAddress(address _new){
+        (bool success,bytes memory data) = _new.call(abi.encodeWithSignature('getProxyAddress()'));
+        (address new_proxy, bool b) = abi.decode(data,(address, bool));
+        require(proxy == new_proxy, "Proxy address is not correct");
+        _;
+    }
+
+    function updateContractAddress(address _new) hasCorrectProxyAddress(_new) external{
+        (bool success, ) = proxy.call(abi.encodeWithSignature('setConsensusAddress(address)', _new));
         require(success,"Error");
     }
 }
